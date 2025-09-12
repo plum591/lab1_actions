2a16be13be9b06f239826e079912ae2483ef0667
plum <sandaara07@vk.com>
2025-09-12 22:02:29 +0700

    second commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 
 int main (int, char **) {
     std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, world!5" << std::endl;
+    std::cout << "Hello, world!" << std::endl;
     return 0;
 }
